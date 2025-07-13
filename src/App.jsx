35a1549bb13b0db7d77b7d33619fc3f0651439ee
Atif Afsar <atifafsar70@gmail.com>
2025-07-13 15:26:31 +0530

    Making ui of gemini.ai

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Components/main/sidebar/Sidebar'
+import Main from './Components/main/main'
+
+const App = () => {
+  return (
+    <>
+     <Sidebar /> 
+     <Main />
+
+    </>
+  )
+}
+
+export default App
